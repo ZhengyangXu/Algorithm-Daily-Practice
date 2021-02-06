3abfddc56a3d99766aa13f2cf0aed56b88249404
ZhengyangXu <zhengyang_xu@sina.cn>
2021-02-06 17:51:56 +0800

    leetcode 87.扰乱字符串

--- 题源分类/LeetCode/LeetCode日刷/python/85.最大矩形.py
+++ 题源分类/LeetCode/LeetCode日刷/python/85.最大矩形.py
@@ -70,26 +70,41 @@
 # @lc code=start
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        if not matrix or not matrix[0]:
+        if not matrix:
             return 0 
         
-        n = len(matrix[0])
-        height = [0] * (n+1)
-        ans = 0 
+        res = 0 
+        m,n = len(matrix),len(matrix[0])
+        heights = [0]*(n+1)
         
         for row in matrix:
             for i in range(n):
-                height[i] = height[i] + 1 if row[i] == '1' else 0 
+                heights[i] = heights[i] + 1 if row[i] == '1' else 0 
             stack = [-1]
             
             for i in range(n+1):
-                while height[i] < height[stack[-1]]:
-                    h = height[stack.pop()]
+                while heights[i] < heights[stack[-1]]:
+                    h = heights[stack.pop()]
                     w = i - 1 - stack[-1]
-                    ans = max(ans,h*w)
+                    res = max(res,h*w)
                 stack.append(i)
-        return ans 
-                    
+            
+        return res 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+  
 # @lc code=end
 
     # def maximalRectangle(self, matrix: List[List[str]]) -> int:

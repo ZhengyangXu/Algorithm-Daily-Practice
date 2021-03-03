2e40a3bd103ed5f1260069b012dd43e168382649
ZhengyangXu <zhengyang_xu@sina.cn>
2021-03-03 20:24:53 +0800

    156.上下翻转二叉树

--- 题源分类/LeetCode/LeetCode日刷/python/156.上下翻转二叉树.py
+++ 题源分类/LeetCode/LeetCode日刷/python/156.上下翻转二叉树.py
@@ -19,7 +19,7 @@
 #   / \
 #  5   2
 #     / \
-#    3   1  
+#    3   1
 # 说明:
 
 # 对 [4,5,2,#,#,3,1] 感到困惑? 下面详细介绍请查看 二叉树是如何被序列化的。
@@ -27,6 +27,7 @@
 # 二叉树的序列化遵循层次遍历规则，当没有节点存在时，'#' 表示路径终止符。
 
 # 这里有一个例子:
+
 
 #    1
 #   / \
@@ -37,49 +38,53 @@
 #      5
 # 上面的二叉树则被序列化为 [1,2,3,#,#,4,#,#,5].
 class TreeNode:
-    def __init__(self,val):
-        self.val = val 
-        self.left = None 
-        self.right = None 
-        
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
 def upsideDownBinaryTree(root):
-    
+
     if not root or not root.left:
-        return root 
-    
-    left = root.left  
-    right = root.right 
-    root.left = None 
-    root.right = None 
+        return root
+
+    left = root.left
+    right = root.right
+    root.left = None
+    root.right = None
     p = upsideDownBinaryTree(left)
-    left.left = right 
-    left.right = root  
-    return p  
-              
+    left.left = right
+    left.right = root
+    return p
+
+
 def upsideDownBinaryTree2(root):
-    
-    cur,pre,nex,tmp = root,None,None,None  
-    while cur: 
-        nex = cur.left  
-        cur.left = tmp  
-        tmp = cur.right  
-        cur.right = pre 
-        pre = cur  
-        cur = nex  
-    return pre           
+
+    cur, pre, nex, tmp = root, None, None, None
+    while cur:
+        nex = cur.left
+        cur.left = tmp
+        tmp = cur.right
+        cur.right = pre
+        pre = cur
+        cur = nex
+    return pre
+
+
 if __name__ == "__main__":
     node1 = TreeNode(1)
     node2 = TreeNode(2)
     node3 = TreeNode(3)
     node4 = TreeNode(4)
     node5 = TreeNode(5)
-    node1.left = node2 
-    node1.right = node3 
-    node2.left = node4 
-    node2.right = node5 
-    
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+
     # x = upsideDownBinaryTree(node1)
     # print(x.val)
-    
+
     y = upsideDownBinaryTree2(node1)
     print(y.val)

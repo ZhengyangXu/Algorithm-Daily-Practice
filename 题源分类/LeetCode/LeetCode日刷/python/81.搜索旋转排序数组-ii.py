4e9a897391d16044e6609130f4c3bffd4134902b
ZhengyangXu <zhengyang_xu@sina.cn>
2021-04-07 20:52:10 +0800

    82

--- 题源分类/LeetCode/LeetCode日刷/python/81.搜索旋转排序数组-ii.py
+++ 题源分类/LeetCode/LeetCode日刷/python/81.搜索旋转排序数组-ii.py
@@ -42,29 +42,59 @@
 # @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-
+        
         left,right = 0,len(nums)-1 
-
-        while left <= right:
-            mid = left + (right - left)//2 
+        
+        while left < right:
+            mid = left+(right-left)//2  
             
-            if nums[mid] == target:
-                return True  
+            if nums[mid] == target: 
+                return True 
             
-            while left < mid and nums[left] == nums[mid]:
+            if nums[left] == nums[right]:
                 left += 1 
+                continue  
             
-            if nums[left] <= nums[mid]:
-                if nums[mid] > target and nums[left] <= target:
-                    right = mid - 1 
+            if nums[mid] <= nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left += 1 
                 else:
-                    left = mid + 1 
-                    
+                    right = mid - 1 
+            
             else:
-                if nums[mid] < target and nums[right] >= target:
-                    left = mid + 1 
-                else:
+                if nums[left]<=target<nums[mid]: 
                     right = mid - 1 
-        return False     
+                else: 
+                    left += 1 
+        if nums[left] == target: 
+            return True 
+        return False 
+            
+            
 # @lc code=end
 
+    # def search(self, nums: List[int], target: int) -> bool:
+
+    #     left,right = 0,len(nums)-1 
+
+    #     while left <= right:
+    #         mid = left + (right - left)//2 
+            
+    #         if nums[mid] == target:
+    #             return True  
+            
+    #         while left < mid and nums[left] == nums[mid]:
+    #             left += 1 
+            
+    #         if nums[left] <= nums[mid]:
+    #             if nums[mid] > target and nums[left] <= target:
+    #                 right = mid - 1 
+    #             else:
+    #                 left = mid + 1 
+                    
+    #         else:
+    #             if nums[mid] < target and nums[right] >= target:
+    #                 left = mid + 1 
+    #             else:
+    #                 right = mid - 1 
+    #     return False     

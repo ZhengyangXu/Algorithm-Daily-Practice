43a380ad8c070f5c2e0c240940b74803de099850
ZhengyangXu <zhengyang_xu@sina.cn>
2021-06-14 18:52:59 +0800

    [374] 猜数字大小

--- 题源分类/LeetCode/LeetCode日刷/python/374.猜数字大小.py
+++ 题源分类/LeetCode/LeetCode日刷/python/374.猜数字大小.py
@@ -82,10 +82,8 @@ class Solution:
     def guessNumber(self, n: int) -> int:
         left, right = 1, n
         while left < right:
-            mid = left + (right - left) // 2
-            if guess(mid) == 0:
-                return mid
-            elif guess(mid) == -1:
+            mid = (right + left) >> 1
+            if guess(mid) <= 0:
                 right = mid
             else:
                 left = mid + 1

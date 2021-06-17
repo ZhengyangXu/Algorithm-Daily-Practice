1bae6401315578dfce02d0b37a5b6f41c0ed380b
ZhengyangXu <zhengyang_xu@sina.cn>
2021-06-17 18:35:59 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

---
+++ 题源分类/剑指offer/python/二刷/剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.py
@@ -0,0 +1,40 @@
+# 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
+# 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+# 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+# 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
+                             q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return root
+        if p.val < q.val:
+            p, q = q, p
+
+        if root.val > p.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        return root
+
+
+# @lc code=end
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
+                             q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return root
+        if p.val < q.val:
+            p, q = q, p
+        while root:
+            if root.val > p.val:
+                root = root.left
+            elif root.val < q.val:
+                root = root.right
+            else:
+                break
+        return root

3017a3d476fc9a857026190ea4fae2911058df59
ZhengyangXu <zhengyang_xu@sina.cn>
2021-08-26 07:11:02 +0800

    binary Search

---
+++ 题源分类/面试面经/java/Interview/src/SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+}
